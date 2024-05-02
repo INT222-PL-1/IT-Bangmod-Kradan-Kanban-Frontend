f0c599f48431a053927212b6f9ceab25bd9cb48a
6MA-606 <zyxma.6666@hotmail.com>
2024-05-02 19:46:04 +0700

    Update: add task feature

--- src/components/TaskModal.vue
+++ src/components/TaskModal.vue
@@ -4,8 +4,8 @@ import BaseModal from '@/components/BaseModal.vue'
 import StatusBadge from './StatusBadge.vue';
 import StatusSelector from './StatusSelector.vue';
 import { useRoute, useRouter } from 'vue-router';
-import { onMounted, ref } from 'vue';
-import { getTaskById } from '@/libs/taskManagement';
+import { onMounted, ref, watch } from 'vue';
+import { createTask, getTaskById } from '@/libs/taskManagement';
 import { useToastStore } from '@/stores/toast';
 
 defineEmits(['clickOk', 'clickClose'])
@@ -45,6 +45,25 @@ onMounted(async () => {
       assignees: '',
       status: 'NO_STATUS'
     }
+
+    watch(() => taskModalData.value?.title.length, (newVal) => {
+      if (newVal > 100) {
+        taskModalData.value.title = taskModalData.value?.title.slice(0, 100).trim()
+      }
+    })
+
+    watch(() => taskModalData.value?.description.length, (newVal) => {
+      if (newVal > 500) {
+        taskModalData.value.description = taskModalData.value?.description.slice(0, 500).trim()
+      }
+    })
+
+    watch(() => taskModalData.value?.assignees.length, (newVal) => {
+      if (newVal > 30) {
+        taskModalData.value.assignees = taskModalData.value?.assignees.slice(0, 30).trim()
+      }
+    })
+
     return
   } else if (taskModalMode.value === 'edit') {
     await fetchTaskData()
@@ -57,6 +76,32 @@ const handleCLickClose = () => {
   router.replace({ name: 'all-task' })
 }
 
+const handleClickConfirm = async () => {
+  // if (taskModalData.value.title.length === 0) {
+  //   toastStore.createToast({
+  //     title: 'Error',
+  //     description: 'Title is required',
+  //     status: 'error'
+  //   })
+  //   return
+  // }
+  const createdTask = await createTask(taskModalData.value)
+  if (createdTask === null) {
+    toastStore.createToast({
+      title: 'Error',
+      description: 'Failed to create task',
+      status: 'error'
+    })
+  } else {
+    toastStore.createToast({
+      title: 'Success',
+      description: `Task created ${createdTask.title} successfully`,
+      status: 'success'
+    })
+  }
+  router.replace({ name: 'all-task' })
+}
+
 </script>
 
 <template>
@@ -65,19 +110,25 @@ const handleCLickClose = () => {
       class="bg-base-100 w-[65rem] max-w-full sm:max-w-[90vw] sm:rounded-xl h-auto lg:h-[40rem] overflow-hidden flex flex-col">
       <div class="text-2xl font-bold p-4 border-b-2 border-base-200 break-words flex-none">
         <span v-if="taskModalMode === 'view'" class="itbkk-title">{{ taskModalData?.title }}</span>
-        <span v-if="taskModalMode === 'add'">New Task</span>
+        <span v-else-if="taskModalMode === 'add'">New Task</span>
       </div>
       <div v-if="taskModalMode === 'add'" class="p-4">
-        <div class="text-lg font-semibold flex-[0]">Title</div>
+        <div class="text-lg font-semibold flex-[0]">Title <span class="text-error">*</span> <span
+            v-if="taskModalMode === 'add'" class="text-sm">({{
+              taskModalData.title.length + '/100'
+            }})</span></div>
         <div class="bg-base-200 px-4 py-2 mt-2 rounded-lg flex-[1]">
-          <textarea v-model="taskModalData.title" placeholder="Enter Task Title"
+          <textarea v-model.trim="taskModalData.title" placeholder="Enter Task Title"
             class="itbkk-description break-words w-full h-full outline-none focus:placeholder:opacity-50 bg-transparent resize-none"></textarea>
         </div>
       </div>
       <div class="grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-2 flex-auto">
         <div>
           <div class="p-4 w-full h-full flex flex-col">
-            <div class="text-lg font-semibold flex-[0]">Description</div>
+            <div class="text-lg font-semibold flex-[0]">Description <span v-if="taskModalMode === 'add'"
+                class="text-sm">({{
+                  taskModalData.description.length + '/500'
+                }})</span></div>
             <div :class="{
               'italic text-[grey] grid place-items-center': !taskModalData?.description,
             }" class="bg-base-200 px-4 py-2 mt-2 rounded-lg flex-[1]">
@@ -93,14 +144,16 @@ const handleCLickClose = () => {
         <div class="flex flex-col justify-between">
           <div>
             <div class="p-4">
-              <div class="text-lg font-semibold">Assignees</div>
+              <div class="text-lg font-semibold">Assignees <span v-if="taskModalMode === 'add'" class="text-sm">({{
+                taskModalData.assignees.length + '/30'
+                  }})</span></div>
               <div v-if="taskModalMode === 'view'" :class="{
                 'italic text-[grey]': !taskModalData?.assignees,
               }" class="itbkk-assignees">
                 {{ taskModalData?.assignees || 'Unassigned' }}
               </div>
               <input v-else-if="taskModalMode === 'add'" v-model="taskModalData.assignees" placeholder="Enter Assignees"
-                class="itbkk-assignees w-[20rem] outline-none focus:placeholder:opacity-50 bg-base-200 px-4 py-2 rounded-xl mt-2" />
+                class="itbkk-assignees w-[20rem] outline-none focus:placeholder:opacity-50 bg-base-200 px-4 py-2 rounded-lg mt-2" />
             </div>
             <div class="p-4">
               <div class="text-lg font-semibold">Status</div>
@@ -147,8 +200,10 @@ const handleCLickClose = () => {
           </button>
         </div>
         <div v-else-if="taskModalMode === 'add'" class="flex gap-2">
-          <button @click="$emit('clickOk')" class="itbkk-button-confirm btn btn-sm btn-success">
-            Ok
+          <button @click="handleClickConfirm"
+            :class="{ 'btn-disabled cursor-not-allowed': taskModalData.title.length < 1 }"
+            class="itbkk-button-confirm btn btn-sm btn-success" :disabled="taskModalData.title.length < 1">
+            Save
           </button>
           <button @click="handleCLickClose" class="itbkk-button-cancel btn btn-sm btn-neutral">
             Cancel
--- src/libs/taskManagement.js
+++ src/libs/taskManagement.js
@@ -4,10 +4,36 @@ export async function getTaskById(taskId) {
   try {
     const res = await fetch(`${SERVER_URL}/tasks/${taskId}`)
     if (res.status === 404) {
-      // console.log(res)
+      console.log(res)
       return null
     }
-    return await res.json()
+    const data = await res.json()
+    console.log(data)
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export async function createTask({ title, description, assignees, status }) {
+  try {
+    const res = await fetch(`${SERVER_URL}/tasks`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        description: description === '' ? null : description,
+        assignees: assignees === '' ? null : assignees,
+        status
+      })
+    })
+
+    if (res.ok) {
+      const data = await res.json()
+      return data
+    } else return null
   } catch (error) {
     console.error(error)
   }
