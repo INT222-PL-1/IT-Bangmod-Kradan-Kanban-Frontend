a74b0a8540b62e02db0f296c7c5b8e4b986c83f8
6MA-606 <zyxma.6666@hotmail.com>
2024-08-16 11:58:20 +0700

    Add: MainLayout and Modify login page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import ToastContainer from './components/ToastContainer.vue'
-import ThemeSwitch from './components/ThemeSwitch.vue'
-import BackToTopButton from './components/BackToTopButton.vue'
-import IconSVG from './components/IconSVG.vue'
 </script>
 
 
 <template>
   <ToastContainer />
-  <BackToTopButton />
-  <div v-if="$route.name === 'login'">
-    <RouterView />
-  </div>
-  <div v-else>
-    <header class="sticky top-0 z-30 flex justify-between items-center bg-base-200 px-6 h-20">
-      <div class="text-xl sm:text-2xl font-bold">IT-Bangmod Kradan Kanban</div>
-      <ThemeSwitch />
-    </header>
-    <nav
-      class="sticky top-20 z-20 px-4 h-12 flex justify-between items-center box-border border-b-2 border-base-300 bg-base-200 drop-shadow-md">
-      <div class="flex gap-4">
-        <div class="flex">
-          <RouterLink :to="{ name: 'all-task' }" exact-active-class="bg-neutral opacity-100 "
-            class="btn btn-ghost btn-sm">
-            <IconSVG iconName="house" :scale="1.25" />Home
-          </RouterLink>
-          <div class="divider divider-horizontal m-0"></div>
-          <RouterLink :to="{ name: 'status-manage' }" exact-active-class="bg-neutral opacity-100"
-            class="itbkk-manage-status btn btn-ghost btn-sm">
-            <IconSVG iconName="sliders2-vertical" :scale="1.25" />Manage Status
-          </RouterLink>
-        </div>
-      </div>
-      <div id="navbar-item-right"></div>
-    </nav>
-    <section class="flex flex-col sm:items-center max-w-full min-h-[calc(100vh-8rem)] h-auto">
-      <RouterView />
-    </section>
-    <footer class="footer footer-center p-4 bg-base-200 text-base-content">
-      <aside>
-        <p>Copyright © 2024 - All right reserved by Dawnbreakers Team</p>
-      </aside>
-    </footer>
-  </div>
+  <RouterView />
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,10 @@
 <script setup>
 import IconSVG from '@/components/IconSVG.vue'
+import ThemeSwitch from '@/components/ThemeSwitch.vue';
 import { ref, watch } from 'vue'
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
 
 const username = ref('')
 const password = ref('')
@@ -9,22 +13,28 @@ watch(username, (newValue) => {
     if (newValue.length >= 50) username.value = newValue.substring(0, 50)
 })
 
+const handleLoginClick = () => {
+    router.push({ name: 'all-task' })
+}
+
 </script>
 
 <template>
+    <div class="fixed top-4 right-4">
+        <ThemeSwitch />
+    </div>
     <main class="h-screen grid place-items-center">
-        <div class="flex flex-col">
+        <div class="flex flex-col bg-base-200 rounded-xl px-14 py-14 shadow-[6px_6px_0px_3px_#0004]">
             <IconSVG iconName="itbkk-logo" :scale="4" size="4rem" />
-            <span class="text-[2rem] my-[1rem]">Login</span>
+            <span class="text-[2rem] mt-[1rem] mb-[0.5rem]">Login</span>
             <span>Welcome to IT-Bangmod Kradan Kanban.</span>
             <span>A productive and easy-to-manage task board.</span>
-            <div class="flex flex-col mt-[1rem]">
+            <div class="flex flex-col mt-[0.5rem]">
                 <input v-model="username" type="username" placeholder="Username"
-                    class="input input-bordered bg-[#252B33] text-[#6F7378] my-[1rem]" />
-
+                    class="input input-bordered border-2 bg-base-100 my-[1rem]" />
                 <input v-model="password" type="password" placeholder="Password"
-                    class="input input-bordered bg-[#252B33] text-[#6F7378]" />
-                <button :disabled="username.length === 0 || password.length === 0"
+                    class="input input-bordered border-2 bg-base-100" />
+                <button @click="handleLoginClick" :disabled="username.length === 0 || password.length === 0"
                     class="mt-[2rem] btn btn-secondary text-base rounded-full">Login</button>
             </div>
         </div>
