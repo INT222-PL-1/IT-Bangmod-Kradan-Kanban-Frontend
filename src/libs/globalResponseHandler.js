c8a379dc3b1e2fcf53d2f415c9e5e0083b8ba6ff
6MA-606 <zyxma.6666@hotmail.com>
2024-09-23 02:53:18 +0700

    Update: enhance README with project description and badges

--- src/libs/globalResponseHandler.js
+++ src/libs/globalResponseHandler.js
@@ -1,10 +1,20 @@
 import router from "@/router"
+import { useToastStore } from "@/stores/toast"
+
 
 const globalResponseHandler = (response) => {
+  const toastStore = useToastStore()
+
   if (response.statusCode === 401) {
     if (router.currentRoute.name === 'login') return
     localStorage.removeItem('itbkk-token')
     router.push({ name: 'login' })
+    toastStore.createToast({
+      title: 'Error',
+      description: 'Your authentication has expired.\nPlease login again.',
+      status: 'error',
+      delay: 500,
+    })
   }
 }
 
