99c54380839deff0b17b7cb200ae7f46ee974973
6MA-606 <zyxma.6666@hotmail.com>
2024-10-04 12:54:08 +0700

    Update: Refactor router to use BoardSelectLayout for board-related routes

---
+++ src/layouts/BoardSelectLayout.vue
@@ -0,0 +1,30 @@
+<script setup>
+import ResponsiveLogo from '@/components/ResponsiveLogo.vue'
+import ThemeSwitch from '@/components/ThemeSwitch.vue'
+import UserMenuButton from '@/components/UserMenuButton.vue'
+import { RouterView } from 'vue-router'
+
+
+</script>
+
+<template>
+  <header class="h-[5rem] top-0 sticky flex justify-between items-center px-6 bg-base-300 shadow-md z-20">
+    <ResponsiveLogo type="compact" />
+    <div class="flex">
+      <ThemeSwitch />
+      <UserMenuButton />
+    </div>
+  </header>
+  <section class="flex flex-col sm:items-center max-w-full min-h-[calc(100vh-4rem)] sm:min-h-[calc(100vh-8rem)] h-auto">
+    <RouterView />
+  </section>
+  <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+    <aside>
+      <p>Copyright © 2024 - All right reserved by Dawnbreakers Team</p>
+    </aside>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
